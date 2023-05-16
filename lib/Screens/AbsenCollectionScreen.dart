88fdd1bcda2008b9ca9332fe4ee118d6373314e4
fajrulalam <71478225+fajrulalam@users.noreply.github.com>
2023-05-16 16:34:40 +0700

    foto sudah expandable dan sudah dibuatkan tampilan bila tidak ada absensi

--- lib/Screens/AbsenCollectionScreen.dart
+++ lib/Screens/AbsenCollectionScreen.dart
@@ -29,141 +29,190 @@ class _AbsenCollectionScreenState extends State<AbsenCollectionScreen> {
         iconTheme: IconThemeData(color: Colors.teal[700]),
       ),
       body: Container(
-        child: GridView.builder(
-          itemCount:
-              widget.absenNgajiKelasList.length, // number of items in the grid
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: MediaQuery.of(context).size.width ~/ 180,
-            mainAxisSpacing: 4.0,
-            crossAxisSpacing: 4.0,
-            childAspectRatio: 0.91,
-          ),
-          itemBuilder: (BuildContext context, int index) {
-            if (widget.absenNgajiKelasList.isEmpty) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            return Container(
-              padding: EdgeInsets.all(12),
-              child: Card(
-                color: _getColor(
-                    widget.absenNgajiKelasList[index].kehadiranAnakSantri!),
-                elevation: 1,
-                //give a rounded corner
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+        child: widget.absenNgajiKelasList.isEmpty
+            ? Center(
+                child: Text(
+                  'Belum ada data absen',
+                  style: GoogleFonts.poppins(
+                      fontSize: 16, fontWeight: FontWeight.w500),
                 ),
-                child: InkWell(
-                  onTap: () {
-                    // Navigator.of(context).push(CustomPageRoute(
-                    //     child: CekAbsensi2_Kelas(
-                    //       kelas: widget.absenNgajiKelasList[index].kelasNgaji,
-                    //       userObject: userObject,
-                    //     )));
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Center(
-                          child: Text(
-                            widget.absenNgajiKelasList[index]
-                                .kehadiranAnakSantri!,
-                            style: GoogleFonts.poppins(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 4,
-                        ),
-                        if (widget.absenNgajiKelasList[index].imagePath! !=
-                            'tidak ada foto')
-                          Container(
-                            margin: EdgeInsets.symmetric(horizontal: 2),
-                            child: Flex(
-                              direction: Axis.horizontal,
-                              children: [
-                                Expanded(
-                                  child: Center(
-                                    child: ClipRRect(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(4)),
-                                      child: Image(
-                                        image: NetworkImage(widget
-                                            .absenNgajiKelasList[index]
-                                            .imagePath!),
+              )
+            : GridView.builder(
+                itemCount: widget
+                    .absenNgajiKelasList.length, // number of items in the grid
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: MediaQuery.of(context).size.width ~/ 180,
+                  mainAxisSpacing: 4.0,
+                  crossAxisSpacing: 4.0,
+                  childAspectRatio: 0.91,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  if (widget.absenNgajiKelasList.isEmpty) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  return Container(
+                    padding: EdgeInsets.all(12),
+                    child: Card(
+                      color: _getColor(widget
+                          .absenNgajiKelasList[index].kehadiranAnakSantri!),
+                      elevation: 1,
+                      //give a rounded corner
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: InkWell(
+                        onTap: () {
+                          if (widget.absenNgajiKelasList[index].imagePath! !=
+                              'tidak ada foto') {
+                            showGeneralDialog(
+                              context: context,
+                              barrierDismissible: true,
+                              barrierLabel: '',
+                              transitionDuration: Duration(milliseconds: 200),
+                              pageBuilder: (context, animation1, animation2) {
+                                return Center(
+                                  child: Stack(
+                                    children: [
+                                      InteractiveViewer(
+                                        child: Image.network(
+                                          widget.absenNgajiKelasList[index]
+                                              .imagePath!,
+                                          fit: BoxFit.contain,
+                                        ),
                                       ),
-                                    ),
+                                      Positioned(
+                                        top: 0,
+                                        right: 0,
+                                        child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.grey,
+                                            shape: CircleBorder(),
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              Navigator.pop(context);
+                                            });
+                                          },
+                                          child: Icon(
+                                            Icons.close,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            );
+                          }
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Center(
+                                child: Text(
+                                  widget.absenNgajiKelasList[index]
+                                      .kehadiranAnakSantri!,
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
                                   ),
                                 ),
-                              ],
-                            ),
-                          )
-                        else
-                          Expanded(
-                              child: Center(
-                            child: Container(
-                              //Curved corners with radius 4
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(4),
-                                color: Colors.grey[200],
                               ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Center(
-                                    child: Icon(
-                                      Icons.no_photography_outlined,
-                                      size: 40,
-                                      color: Colors.grey[400],
+                              SizedBox(
+                                height: 4,
+                              ),
+                              if (widget
+                                      .absenNgajiKelasList[index].imagePath! !=
+                                  'tidak ada foto')
+                                Container(
+                                  margin: EdgeInsets.symmetric(horizontal: 2),
+                                  child: Flex(
+                                    direction: Axis.horizontal,
+                                    children: [
+                                      Expanded(
+                                        child: Center(
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(4)),
+                                            child: Image(
+                                              image: NetworkImage(widget
+                                                  .absenNgajiKelasList[index]
+                                                  .imagePath!),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              else
+                                Expanded(
+                                    child: Center(
+                                  child: Container(
+                                    //Curved corners with radius 4
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(4),
+                                      color: Colors.grey[200],
+                                    ),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.stretch,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Center(
+                                          child: Icon(
+                                            Icons.no_photography_outlined,
+                                            size: 40,
+                                            color: Colors.grey[400],
+                                          ),
+                                        ),
+                                        Center(
+                                          child: Text(
+                                            'Tidak ada foto',
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w400),
+                                          ),
+                                        )
+                                      ],
                                     ),
                                   ),
-                                  Center(
-                                    child: Text(
-                                      'Tidak ada foto',
-                                      style: GoogleFonts.poppins(
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w400),
+                                )),
+                              SizedBox(
+                                height: 4,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    dateFormat(widget
+                                        .absenNgajiKelasList[index].timestamp!),
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
                                     ),
-                                  )
+                                  ),
                                 ],
                               ),
-                            ),
-                          )),
-                        SizedBox(
-                          height: 4,
+                            ],
+                          ),
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              dateFormat(
-                                  widget.absenNgajiKelasList[index].timestamp!),
-                              style: GoogleFonts.poppins(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
+                  );
+                },
               ),
-            );
-          },
-        ),
       ),
     );
   }
